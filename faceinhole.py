9c0e95c3fa650297017aff612555bc30a4cba2bc
M. Jirik <miroslav.jirik@gmail.com>
2015-05-14 11:10:51 +0200

    pygame used

--- faceinhole.py
+++ faceinhole.py
@@ -18,36 +18,71 @@ import cv2
 from PIL import Image
 import scipy
 import scipy.misc
+import pygame
 
 
 class FaceInHole():
+    def __init__(self):
+        """TODO: Docstring for __init__.
+        :returns: TODO
+
+        """
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((640,480))         # vytvoření okna s nastavením jeho velikosti
+        pygame.display.set_caption("Example")               # nastavení titulku okna
+        
+        self.background = pygame.Surface(self.screen.get_size())      # vytvoření vrstvy pozadí
+        self.background = self.background.convert()                   # převod vrstvy do vhodného formátu
+        self.background.fill((0,0,255))                 
+        self.clock = pygame.time.Clock()                         # časování
+        self.keepGoing = True 
+
     def run(self):
         cap = cv2.VideoCapture(0)
-        imurl = 'images/plakat.jpg'
-
-        # im = Image.open('images/mona.png')
-        imscene = scipy.misc.imread(imurl)[:, :, ::-1]
+        imurl = 'images/plakat_full.jpg'
+        imurl2 = 'images/D6-12.png'
+        imscene = scipy.misc.imread(imurl)# [:, :, ::-1]
+        imscene2 = pygame.image.load(imurl2)
         # im = Image.open(imurl)
         # fgbg = cv2.createBackgroundSubtractorMOG()
-        fgbg = cv2.BackgroundSubtractorMOG()
+        fgbg = cv2.BackgroundSubtractorMOG2()
 
-        while(1):
+        while(self.keepGoing):
             ret, frame = cap.read()
-            npframe = np.asarray(frame)
-            print npframe.shape
+            npframe = np.asarray(frame)[:, :, ::-1]
             imscene = fill_to_shape(imscene, npframe.shape)
             fgmask = fgbg.apply(frame)
+            # print npframe.shape
+            print 'mask'
+            print np.max(fgmask)
+            print np.min(fgmask)
             newframe = immerge(npframe, imscene, fgmask, 255-fgmask)
 
-            # vis2 = cv.CreateMat(h, w, cv.CV_32FC3)
-            # vis0 = cv.fromarray(vis)
-            # cv.CvtColor(vis0, vis2, cv.CV_GRAY2BGR)
-            # cv2.imshow('frame',fgmask)
-            cv2.imshow('frame', newframe)
-            # cv2.imshow('frame',frame)
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:
-                break
+            # # cv2.imshow('frame',fgmask)
+            # cv2.imshow('frame', newframe)
+            # # cv2.imshow('frame', imscene)
+            # # cv2.imshow('frame',frame)
+            # k = cv2.waitKey(30) & 0xff
+            # if k == 27:
+            #     break
+            self.clock.tick(30)                                  # omezení maximálního počtu snímků za sekundu
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:               # probíhá událost požadující zavření okna programu?
+                    keepGoing = False                       # ukončení hlavní smyčky
+        
+            # scipy.misc.imresize(new_frame, )
+            # backp = pygame.surfarray.pixels2d(self.background)
+
+            # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
+            newframe = np.rot90(newframe, 1)
+            sf_newframe = makesurf(newframe)
+            
+            self.screen.blit(sf_newframe, (0,0))                  # přidání pozadí k vykreslení na pozici 0, 0
+            self.screen.blit(imscene2, (0,0))                  # přidání pozadí k vykreslení na pozici 0, 0
+            # self.screen.blit(self.background, (0,0))                  # přidání pozadí k vykreslení na pozici 0, 0
+            # self.screen.blit(text, textRect)                     # přidání textu k vykreslení na střed
+            pygame.display.flip()        
 
         cap.release()
         cv2.destroyAllWindows()
@@ -65,16 +100,8 @@ def fill_to_shape(im, shape):
 
 def immerge(im1, im2, mask1, mask2):
     # scipy.misc.imresize
-    print im1.shape
-    print im2.shape
-    print mask1.shape
-    print mask2.shape
-    print np.max(mask1)
-    print np.min(mask1)
-    print np.max(mask2)
-    print np.min(mask2)
-    # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
     if len(im1.shape) != len(mask1.shape):
+        print "mask is binary"
         if len(mask1.shape) == 2:
             masknew = np.zeros(im1.shape, dtype=mask1.dtype)
             masknew[:, :, 0] = mask1
@@ -91,10 +118,25 @@ def immerge(im1, im2, mask1, mask2):
             mask2 = masknew
 
 
-    return (im1 * mask1 * 1.0/255) + (im2 * mask2 * 1.0/255)
+    retim = (im1 * (mask1 / 255.0)) + (im2 * (mask2 / 255.0))
+    print 'retim'
+    print np.max(retim)
+    print np.min(retim)
+    # print retim.dtype
+    return retim.astype(np.uint8)
 
 def loop():
     pass
+
+def makesurf(pixels):
+    try:
+        surf = pygame.surfarray.make_surface(pixels)
+    except IndexError:
+        (width, height, colours) = pixels.shape
+        surf = pygame.display.set_mode((width, height))
+        pygame.surfarray.blit_array(surf, pixels)
+    return surf
+    
 
 def main():
     logger = logging.getLogger()
